f6f7aadc061992ece5f89a9246e9732b187e4a63
player-geun <player_geun@naver.com>
2022-05-07 15:55:21 +0900

    mvc #2 - Request Mapping
    다양한 요청 매핑에 대해서 학습한다.

---
+++ src/main/java/hello/springmvc/basic/MappingController.java
@@ -0,0 +1,76 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.print.attribute.standard.Media;
+
+@Slf4j
+@RestController
+public class MappingController {
+
+    @RequestMapping("/hello-basic")
+    public String helloBasic() {
+
+        log.info("helloBasic");
+
+        return "ok";
+    }
+
+    @RequestMapping(value = "/mapping-get-v1", method = RequestMethod.GET)
+    public String mappingGetV1() {
+
+        log.info("mappingGetV1");
+
+        return "ok";
+    }
+
+    @GetMapping(value = "/mapping-get-v2")
+    public String mappingGetV2() {
+
+        log.info("mapping-get-v2");
+
+        return "ok";
+    }
+
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable("userId") String data) {
+
+        log.info("mappingPath userId={}", data);
+
+        return "ok";
+    }
+
+    @GetMapping("/mapping/users/{userId}/orders/{orderId}")
+    public String mappingPath(@PathVariable String userId, @PathVariable Long orderId) {
+
+        log.info("mappingPath userId={}, orderId={}", userId, orderId);
+
+        return "ok";
+    }
+
+    @GetMapping(value = "/mapping-param", params = "mode=debug")
+    public String mappingParam() {
+
+        log.info("mappingParam");
+
+        return "ok";
+    }
+
+    @PostMapping(value = "/mapping-consume", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String mappingConsumes() {
+
+        log.info("mappingConsumes");
+
+        return "ok";
+    }
+
+    @PostMapping(value = "/mapping-produce", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String mappingProduces() {
+
+        log.info("mappingProduces");
+
+        return "ok";
+    }
+}
